964f965067710f6a9211b27e8a0568ef2d63d1e5
Subhamay-Dey <ncdey1966@gmail.com>
2024-12-21 00:44:08 +0530

    small bug fixed ✅

--- frontend/src/components/auth/LoginModal.tsx
+++ frontend/src/components/auth/LoginModal.tsx
@@ -40,9 +40,8 @@ function LoginModal() {
                     className='mr-4'
                     width={25}
                     height={25}
-                    alt='google_logo'
+                    alt='Continue with Google'
                 >
-                    Continue with Google
                 </Image>
             </Button>
         </DialogContent>
