b323655e3b09e31f0852da69e7dba01f1dcfc9cf
Subhamay-Dey <ncdey1966@gmail.com>
2024-12-21 05:39:13 +0530

    Extracted the user's name and image from session and passed that data to dashNavbar instead of the hard coded user data ✅

--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -1,10 +1,18 @@
 import DashNavbar from '@/components/dashboard/dashNavbar'
+import { getServerSession } from 'next-auth'
 import React from 'react'
+import { authOptions, CustomSession } from '../api/auth/[...nextauth]/options'
 
-function Dashboard() {
+async function Dashboard() {
+
+  const session:CustomSession|null = await getServerSession(authOptions);
+
   return (
     <div>
-      <DashNavbar/>
+      <DashNavbar
+        name={session?.user?.name!}
+        image={session?.user?.image ?? undefined}
+      />
     </div>
   )
 }
