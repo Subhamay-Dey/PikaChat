d85df4c8c91281e11cff7564ca104c9c3a3d4a4c
Subhamay-Dey <ncdey1966@gmail.com>
2024-12-22 20:43:19 +0530

    ChatGroupController api successfully done ✅ and also made the port route with authMiddleware ✅

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,8 +1,14 @@
 import { Router } from "express";
 import AuthController from "../controllers/AuthController.js";
+import authMiddleware from "../middlewares/AuthMiddleware.js";
+import ChatGroupController from "../controllers/ChatGroupController.js";
 
 const router = Router()
 
+//Auth Route
 router.post("/auth/login", AuthController.login)
+
+//Chat Group Route
+router.post("/chat-group", authMiddleware, ChatGroupController.store)
 
 export default router
