8a1a3f97219e063ffe6518ac91d82dfc1e4ce4d0
Subhamay-Dey <ncdey1966@gmail.com>
2025-01-12 19:01:05 +0530

    Update routes in index.ts: remove authMiddleware to the ChatGroupController.show

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -3,16 +3,16 @@ import AuthController from "../controllers/AuthController.js";
 import authMiddleware from "../middlewares/AuthMiddleware.js";
 import ChatGroupController from "../controllers/ChatGroupController.js";
 
-const router = Router()
+const router = Router();
 
 //Auth Route
-router.post("/auth/login", AuthController.login)
+router.post("/auth/login", AuthController.login);
 
 //Chat Group Route
-router.post("/chat-group", authMiddleware, ChatGroupController.store)
-router.get("/chat-group", authMiddleware, ChatGroupController.index)
-router.get("/chat-group:id", authMiddleware, ChatGroupController.show)
-router.put("/chat-group:id", authMiddleware, ChatGroupController.update)
-router.delete("/chat-group:id", authMiddleware, ChatGroupController.destroy)
+router.post("/chat-group", authMiddleware, ChatGroupController.store);
+router.get("/chat-group", authMiddleware, ChatGroupController.index);
+router.get("/chat-group:id", ChatGroupController.show);
+router.put("/chat-group:id", authMiddleware, ChatGroupController.update);
+router.delete("/chat-group:id", authMiddleware, ChatGroupController.destroy);
 
-export default router
+export default router;
