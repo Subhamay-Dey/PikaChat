88b9e285951b725de9b177be7bcdbf89cdde476e
Subhamay-Dey <ncdey1966@gmail.com>
2025-01-12 19:16:05 +0530

    intrigated the fetchChatGroup in the chat/[id] page.tsx and also added edge cases ✅

--- frontend/src/app/chat/[id]/page.tsx
+++ frontend/src/app/chat/[id]/page.tsx
@@ -1,13 +1,23 @@
 import ChatBase from '@/components/chat/ChatBase';
 import { fetchChatGroup } from '@/fetch/groupFetch';
+import { notFound } from 'next/navigation';
 import React from 'react'
 
 async function chat({params}:{params:{id:string}}) {
 
+  if(params.id.length != 36) {
+    return notFound();
+  }
+  
+  const group:ChatGroupType | null = await fetchChatGroup(params.id)
+
+  if(group == null) {
+    return notFound();
+  }
+
   return (
     <div>
-        <h1>Hello, I am Chat</h1>
-        <ChatBase groupId={params.id}/>
+      <ChatBase groupId={params.id}/>
     </div>
   )
 }
