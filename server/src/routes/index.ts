c05047c4db80d9d53cde946ff12893ce9fe63067
Subhamay-Dey <ncdey1966@gmail.com>
2025-01-13 19:32:15 +0530

    declared the two routes in index.ts and also made them public ✅

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -2,17 +2,22 @@ import { Router } from "express";
 import AuthController from "../controllers/AuthController.js";
 import authMiddleware from "../middlewares/AuthMiddleware.js";
 import ChatGroupController from "../controllers/ChatGroupController.js";
+import ChatGroupUserController from "../controllers/ChatGroupUserController.js";
 
 const router = Router();
 
-//Auth Route
+// Auth Route
 router.post("/auth/login", AuthController.login);
 
-//Chat Group Route
+// Chat Group Route
 router.post("/chat-group", authMiddleware, ChatGroupController.store);
 router.get("/chat-group", authMiddleware, ChatGroupController.index);
 router.get("/chat-group:id", ChatGroupController.show);
 router.put("/chat-group:id", authMiddleware, ChatGroupController.update);
 router.delete("/chat-group:id", authMiddleware, ChatGroupController.destroy);
+
+// Chat Group Users
+router.get("/chat-group-users", ChatGroupUserController.index);
+router.get("/chat-group-users", ChatGroupUserController.store);
 
 export default router;
