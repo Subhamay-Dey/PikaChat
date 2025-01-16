3c36640cb6be6f92190a0af8c64c8d0b4f089a96
Subhamay-Dey <ncdey1966@gmail.com>
2025-01-16 20:54:27 +0530

    Added Chats component and implemented local storage for chat user data ✅

--- frontend/src/components/chat/ChatBase.tsx
+++ frontend/src/components/chat/ChatBase.tsx
@@ -6,39 +6,28 @@ import {v4 as uuidV4} from 'uuid'
 import ChatSidebar from './ChatSidebar'
 import ChatNav from './ChatNav'
 import ChatUserDialog from './ChatUserDialog'
+import Chats from './Chats'
 
 export default function ChatBase({group, users}:{group:ChatGroupType, users: Array<GroupChatUserType> | []}) {
-
-    // let socket = useMemo(() => {
-    //     const socket = getSocket();
-    //     socket.auth = {
-    //         room: groupId,
-    //     }
-    //     return socket.connect();
-    // }, []);
-
-    // useEffect(() => {
-
-    //     socket.on('message', (data:any) => {
-    //         console.log("The socket message is:", data);
-    //     })
-
-    //     return () => {
-    //         socket.close();
-    //     }
-    // },[])
-
-    // const handleClick = () => {
-    //     socket.emit("message", {name:"Subho", id:uuidV4()});
-    // }
     
     const [open, setOpen] = useState(true)
+    const [chatUser, setChatUser] = useState<GroupChatUserType>()
+
+    useEffect(() => {
+      const data = localStorage.getItem(group.id)
+      if(data) {
+        const passData = JSON.parse(data)
+        setChatUser(passData)
+      }
+    }, [group.id])
+    
 
   return (
     <div className='flex'>
         <ChatSidebar users={users}/>
         <div className='w-full md:w-4/5 bg-gradient-to-b from-gray-50 to-white'>
         {open ? <ChatUserDialog open={open} setOpen={setOpen} group={group}/> : <ChatNav chatGroup={group} users={users}/>}
+        <Chats group={group} chatUser={chatUser} oldMessages={}/>
         </div>
     </div>
   )
