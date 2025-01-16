3a380159f70538a42119609e024529946ada86b7
Subhamay-Dey <ncdey1966@gmail.com>
2025-01-16 21:59:13 +0530

    Added import of prisma and creation of chat in database on message event ✅

--- server/src/socket.ts
+++ server/src/socket.ts
@@ -1,4 +1,5 @@
 import { Server, Socket } from "socket.io";
+import prisma from "./config/db.config.js";
 
 interface CustomSocket extends Socket {
     room?: string
@@ -21,9 +22,12 @@ export function setupSocket(io: Server) {
 
         console.log('The socket connected..', socket.id);
 
-        socket.on("message", (data) => {
-            console.log("Server side message", data);
+        socket.on("message", async (data) => {
+            // console.log("Server side message", data);
             // socket.emit("message", data);
+            await prisma.chats.create({
+                data: data
+            })
             socket.to(socket.room).emit("message", data)
         });
 
