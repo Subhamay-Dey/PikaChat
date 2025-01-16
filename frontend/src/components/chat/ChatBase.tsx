350bfd4d6a3adc55ceebdd604fce0d3d4a6c2870
Subhamay-Dey <ncdey1966@gmail.com>
2025-01-16 20:57:47 +0530

    Added oldMessages prop to ChatBase component

--- frontend/src/components/chat/ChatBase.tsx
+++ frontend/src/components/chat/ChatBase.tsx
@@ -8,7 +8,7 @@ import ChatNav from './ChatNav'
 import ChatUserDialog from './ChatUserDialog'
 import Chats from './Chats'
 
-export default function ChatBase({group, users}:{group:ChatGroupType, users: Array<GroupChatUserType> | []}) {
+export default function ChatBase({group, users, oldMessages}:{group:ChatGroupType, users: Array<GroupChatUserType> | []}, oldMessages: Array<MessageType> | []) {
     
     const [open, setOpen] = useState(true)
     const [chatUser, setChatUser] = useState<GroupChatUserType>()
@@ -27,7 +27,7 @@ export default function ChatBase({group, users}:{group:ChatGroupType, users: Arr
         <ChatSidebar users={users}/>
         <div className='w-full md:w-4/5 bg-gradient-to-b from-gray-50 to-white'>
         {open ? <ChatUserDialog open={open} setOpen={setOpen} group={group}/> : <ChatNav chatGroup={group} users={users}/>}
-        <Chats group={group} chatUser={chatUser} oldMessages={}/>
+        <Chats group={group} chatUser={chatUser} oldMessages={oldMessages}/>
         </div>
     </div>
   )
