1404ed8cc4c212ebaaf55f631868b303a6e8dd5c
Subhamay-Dey <ncdey1966@gmail.com>
2025-02-01 18:26:14 +0530

    Add custom error reporter for validation errors ✅

--- server/src/validations/validation.ts
+++ server/src/validations/validation.ts
@@ -1,4 +1,7 @@
 import vine from "@vinejs/vine";
+import { CustomErrorReporter } from "./errorReporter.js";
+
+vine.errorReporter = () => new CustomErrorReporter()
 
 export const LocationdataSchema = vine.object({
     city: vine.string().minLength(2).maxLength(191),
