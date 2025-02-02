9bb6481a6b66244bae787cce26854b3f7bb458fb
Subhamay-Dey <ncdey1966@gmail.com>
2025-02-03 01:03:29 +0530

    Changed the request type to query as the input will be as query on which the prisma will use those inputs to find the user with the same field data ✅

--- server/src/controllers/UserSearchController.ts
+++ server/src/controllers/UserSearchController.ts
@@ -1,30 +1,35 @@
 import prisma from "../config/db.config.js";
+import { Request, Response } from "express";
 
 interface SearchType {
     city?:string,
     state?:string,
-    nationality:string
+    nationality?:string
 }
 
 class UserSearchController {
-    static async search(req: any, res: any) {
+    static async search(req: Request, res: Response) {
 
         try {
-            const body:SearchType = req.body;
+            const {city, state, nationality} = req.query as SearchType;
+
+            if (!nationality) {
+                return res.status(400).json({ message: "Nationality is required" });
+              }
 
             const users = await prisma.user.findMany({
                 where:{
                     nationality: {
-                        equals: body.nationality,
+                        equals: nationality,
                         mode: "insensitive",
                     },
-                    ...(body.city && {city:{
-                        equals: body.city,
+                    ...(city && {city:{
+                        equals: city,
                         mode: "insensitive",
                         }
                     }),
-                    ...(body.state && {state:{
-                        equals: body.state,
+                    ...(state && {state:{
+                        equals: state,
                         mode: "insensitive",
                         }
                     }),
@@ -36,7 +41,7 @@ class UserSearchController {
         res.status(200).json({message:"User fetched successfully", users});
         } catch (error) {
             console.log(error.message);
-            res.status(400).json({message:"Something went wrong"})
+            res.status(500).json({message:"Something went wrong"})
         }
 
     }
