207ae523d1008fc2a28725768fe4f46132ea79fc
Subhamay-Dey <ncdey1966@gmail.com>
2025-02-02 23:59:37 +0530

    Implement search form submission with success and error handling

--- frontend/src/app/search/page.tsx
+++ frontend/src/app/search/page.tsx
@@ -5,6 +5,7 @@ import { Card } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import React, { useState } from "react";
+import { toast } from "sonner";
 
 function page() {
 
@@ -19,10 +20,40 @@ function page() {
         nationality,
     }
 
+    const submitHandler = async(e: React.FormEvent) => {
+        e.preventDefault();
+
+        console.log("The search payload is", payload);
+
+        try {
+            setLoading(true);
+
+            const response = await fetch("http://localhost:8000/api/search", {
+                method: "GET",
+                body: JSON.stringify({
+                    city,
+                    state,
+                    nationality,
+                })
+            });
+
+            const data = await response.json();
+
+            if(!response.ok) throw new Error(data.message || "Form submission failed")
+            console.log(data);
+            setLoading(false);
+            toast.success(data?.message);
+
+        } catch (error) {
+            setLoading(false);
+            console.log(error);
+        }
+    }
+
 
   return (
     <div className="w-full h-screen flex justify-center items-center">
-      <form>
+      <form onSubmit={submitHandler}>
         <div className="gap-10">
             <Label htmlFor="city">City/Town Name</Label>
             <Card className="w-[450px] h-[50px]">
