b34f3ea81382ccd3fc75131bc81cf9da0e8f1811
Subhamay-Dey <ncdey1966@gmail.com>
2025-02-05 22:28:04 +0530

    Enhance socket functionality to support direct and group messaging with user identification ✅

--- server/src/socket.ts
+++ server/src/socket.ts
@@ -2,32 +2,49 @@ import { Server, Socket } from "socket.io";
 import prisma from "./config/db.config.js";
 
 interface CustomSocket extends Socket {
-    room?: string
+    room?: string;
+    userId?: number;
 }
 
 export function setupSocket(io: Server) {
 
     io.use((socket:CustomSocket, next) => {
         const room = socket.handshake.auth.room || socket.handshake.headers.room;
-        if(!room) {
+        const userId = socket.handshake.auth.userId;
+        if(!room && !userId) {
             return next(new Error("Invalid room"))
         }
         socket.room = room;
+        socket.userId = userId;
         next()
-    })
+    });
 
     io.on('connection', (socket: CustomSocket) => {
 
-        socket.join(socket.room)
+        if (socket.room) {
+            socket.join(socket.room);
+        } else if (socket.userId) {
+            socket.join(`user-${socket.userId}`);
+        }
 
         console.log('The socket connected..', socket.id);
 
-        socket.on("message", async (data) => {
+        // Group Message
+        socket.on("groupMessage", async (data) => {
 
             await prisma.chats.create({
                 data: data
             })
-            socket.to(socket.room).emit("message", data)
+            socket.to(socket.room).emit("groupMessage", data)
+        });
+
+        // Direct Message
+        socket.on("directMessage", async ({ senderId, receiverId, message }) => {
+            const chatData = await prisma.directMessage.create({
+                data: { senderId, receiverId, message },
+            });
+
+            io.to(`user-${receiverId}`).emit("directMessage", chatData);
         });
 
         socket.on("disconnect", () => {
